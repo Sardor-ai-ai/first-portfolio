646e77fea4bc04839e0136d06970ec23037fae97
Sardor-ai-ai <sardor86pro@gmail.com>
2026-05-05 21:39:23 +0500

    Add files via upload

---
+++ git/index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile('index.html', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    } 
+    else if (req.url === '/style.css') {
+        fs.readFile('style.css', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.write(data);
+            res.end();
+        });
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server ishlayapti: http://localhost:3000 🚀');
+});
